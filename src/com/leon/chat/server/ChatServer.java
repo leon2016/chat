e9fdd0beec7514e01f11e1aa3c9512a47e855ad5
leon <1754235453@qq.com>
2017-06-02 00:22:34 +0800

    多线程的ServerSocket,为每一个客户端请求分配一个工作线程。

--- src/com/leon/chat/server/ChatServer.java
+++ src/com/leon/chat/server/ChatServer.java
@@ -2,6 +2,7 @@ package com.leon.chat.server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,22 +11,32 @@ import com.leon.chat.thread.ChatWriterThread;
 
 public class ChatServer {
     public static void main(String[] args) throws Exception {
-        
-        System.out.println("服务器启动,等待用户连接...");
-        // 设置sever端的链接
-        ServerSocket s = new ServerSocket(8888);
-        Socket s1 = s.accept();// sever等待链接
-        String clientIP = s1.getInetAddress().getHostAddress();
-        System.out.println(clientIP+"上线了");
-        // 实例化输入流
-        DataInputStream dis = new DataInputStream(s1.getInputStream());
-        // 实例化输出流
-        DataOutputStream dos = new DataOutputStream(s1.getOutputStream());
-        // 实例化两个线程的对象
-        Thread chatReadThread = new ChatReadThread(dis,clientIP);
-        Thread chatWriterThread = new ChatWriterThread(dos);
-        // 启动线程
-        chatReadThread.start();
-        chatWriterThread.start();
+        ChatServer chatServer = new ChatServer();
+        chatServer.server();
+    }
+    public void server(){
+        System.out.println("服务器启动,端口号：8888,等待用户连接...");
+        try {
+            // 设置sever端的链接
+            ServerSocket s = new ServerSocket(8888);
+            while(true){
+                Socket s1 = s.accept();// sever等待链接
+                String clientIP = s1.getInetAddress().getHostAddress();
+                System.out.println(clientIP+"上线了");
+                // 实例化输入流
+                DataInputStream dis = new DataInputStream(s1.getInputStream());
+                // 实例化输出流
+                DataOutputStream dos = new DataOutputStream(s1.getOutputStream());
+                // 实例化两个线程的对象
+                Thread chatReadThread = new ChatReadThread(dis,clientIP);
+                Thread chatWriterThread = new ChatWriterThread(dos);
+                // 启动线程
+                chatReadThread.start();
+                chatWriterThread.start();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
     }
 }
